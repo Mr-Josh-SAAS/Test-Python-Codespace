2b97ca1e278fb48cd13909d0c36a685c887b4c7e
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2026-09-16 22:13:03 +0000

    noVNC autostart try 3

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -12,7 +12,3 @@ echo "Starting noVNC setup:"
 
 sudo apt-get update
 sudo apt-get install -y novnc websockify
-nohup /usr/share/novnc/utils/novnc_proxy \
-	--vnc localhost:5901 \
-	--listen 0.0.0.0:6080 \
-	>/tmp/novnc.log 2>&1 &
